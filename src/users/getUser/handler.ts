13dc4d29478c2f7886f561d90c39b9f66a708e00
Alaa <alaa0602@hotmail.com>
2023-08-02 23:25:26 +0300

    added getUser handler

---
+++ src/users/getUser/handler.ts
@@ -0,0 +1,37 @@
+import { middyfy } from "@lib/middleware";
+
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+
+const ddb = new DocumentClient({
+    region: "us-east-1"
+})
+
+export default middyfy(async (event) => {
+    const { name, email } = event.body;
+
+    if (!name || name.length === 0) {
+        return {
+            statusCode: 422,
+            body: JSON.stringify({
+                error: {
+                    title: "ValidatationError",
+                    message: "Name is required"
+                }
+            })
+        }
+    }
+    console.log({ event });
+
+    await ddb.get({
+        TableName: process.env.TABLE_NAME!,
+        Key: {
+            "pk": name,
+            "sk": email
+        }
+    }).promise();
+
+    return {
+        statusCode: 200,
+        body: null
+    }
+});
