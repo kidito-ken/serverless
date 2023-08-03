e01d98865e47591b3801013c23478f0480cf0b56
Alaaa1 <alaa0602@hotmail.com>
2023-08-03 12:21:57 +0300

    refactored editUser to sdk v3

--- src/users/editUser/handler.ts
+++ src/users/editUser/handler.ts
@@ -1,43 +1,42 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb';
 import { middyfy } from "@lib/middleware";
 
-import { DocumentClient } from 'aws-sdk/clients/dynamodb';
-
-const ddb = new DocumentClient({
-    region: "us-east-1"
-})
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 export default middyfy(async (event) => {
     const { name, email, newEmail } = event.body;
 
-    if (!name || name.length === 0) {
+    if (!name || name.length === 0 || !email || email.length === 0 || !newEmail || newEmail.length === 0) {
         return {
             statusCode: 422,
             body: JSON.stringify({
                 error: {
                     title: "ValidatationError",
-                    message: "Name is required"
+                    message: "required information is required"
                 }
             })
         }
     }
     console.log({ event });
 
-    await ddb.update({
+    const command = new UpdateCommand({
         TableName: process.env.TABLE_NAME!,
         Key: {
             "pk": name,
             "sk": email
         },
-        AttributeUpdates: {
-            "email": {
-                Action: "PUT",
-                Value: newEmail
-            }
-        }
-    }).promise();
+        UpdateExpression: "set email = :newEmail",
+        ExpressionAttributeValues: {
+            ":newEmail": newEmail
+        },
+        ReturnValues: "ALL_NEW"
+    });
 
+    const response = await docClient.send(command);
     return {
         statusCode: 200,
-        body: null
+        body: "User Updated Successfully!"
     }
 });
