51471fbc1021171864dec5e1d7edee7def224e49
kidito-ken <95145835+kidito-ken@users.noreply.github.com>
2023-08-10 15:15:03 +0300

    added athorizer

--- src/user-service-stack/src/editUser/handler.ts
+++ src/user-service-stack/src/editUser/handler.ts
@@ -1,80 +1,69 @@
-import { PublishCommand } from '@aws-sdk/client-sns';
-import { SNSClient } from '@aws-sdk/client-sns';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb';
 import { middyfy } from "@lib/middleware";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb"
+import { PublishCommand, PublishCommandInput, SNSClient } from "@aws-sdk/client-sns"
 
-const snsClient = new SNSClient({});
-const docClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+const snsClient = new SNSClient({
+    region: 'us-east-1'
+  });
 
-export default middyfy(async (event) => {
-    // if (!event.requestContext.authorizer) {
-    //     return {
-    //         statusCode: 401,
-    //         body: JSON.stringify({
-    //             error: {
-    //                 title: 'Unauthorized',
-    //                 message: 'User is not authenticated',
-    //             },
-    //         }),
-    //     };
-    // }
-    const { name, email, newEmail } = event.body;
+async function sendMessage() {
 
-    if (!name || name.length === 0 || !email || email.length === 0 || !newEmail || newEmail.length === 0) {
-        return {
-            statusCode: 422,
-            body: JSON.stringify({
-                error: {
-                    title: "ValidationError",
-                    message: "Required info missing",
-                },
-            }),
-        };
-    }
-    console.log({ event });
 
-    const command = new UpdateCommand({
-        TableName: process.env.TABLE_NAME!,
-        Key: {
-            "pk": name,
-            "sk": email,
-        },
-        UpdateExpression: "set email = :newEmail",
-        ExpressionAttributeValues: {
-            ":newEmail": newEmail,
-        },
-        ReturnValues: "ALL_NEW",
+    const messageBody = JSON.stringify({
+        message: "User Updated",
+        userStatus: [ "updated" ]
     });
-
-    const response = await docClient.send(command);
-
-
-    await snsClient.send(new PublishCommand({
-        Message: "User Created!",
-        TopicArn: process.env.SNS_ARN!,
-        MessageAttributes: {
-          MessageName: {
-            DataType: 'String',
-            StringValue: 'Update',
-          }
-        }
-      }));
-    // const messageParameters = {
-    //     Message: "User Updated!",
-    //     TopicArn: process.env.SNS_ARN!,
-    //     MessageAttributes: {
-    //         MessageName: {
-    //             DataType: 'String',
-    //             StringValue: 'Update',
-    //         },
-    //     },
-    // };
-
-    // await snsClient.send(new PublishCommand(messageParameters));
-
+  
+    const params: PublishCommandInput = {
+        TopicArn: process.env.SNS_ARN,
+        Message: messageBody, 
+    }
+  
+    const message = new PublishCommand(params)
+  
+    if (message) {
+        console.log("worked")
+    }else {
+        console.log("didnt work")
+    }
+  
+    const response = await snsClient.send(message)
+    console.log("This is response message id: ", response.MessageId)
+  
     return {
-        statusCode: 200,
-        body: "User Updated Successfully!",
-    };
-});
+        response
+    }
+  }
+  const client = new DynamoDBClient({});
+  const docClient = DynamoDBDocumentClient.from(client);
+  
+  export default middyfy(async (event) => {
+    const { email } = event.pathParameters;
+    const { phoneNumber } = event.body;
+  
+    const command = new UpdateCommand({
+      TableName: process.env.TABLE_NAME!,
+      Key: { 
+        "pk": "user", 
+        "sk": email
+      },
+      UpdateExpression: "SET phoneNumber = :phoneNumber",
+      ExpressionAttributeValues: {
+          ":phoneNumber": phoneNumber,
+      },
+      ReturnValues: "UPDATED_NEW"
+    })
+  
+    const response = await docClient.send(command);
+    console.log({ event });
+  
+    const message = await sendMessage()
+  
+    console.log(message)
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    }
+  });
